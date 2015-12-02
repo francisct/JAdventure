b21c685ddfda26a2b9ab1a31b9ee3ee4b253d2d2
francis cote-tremblay <franciscotetremblay@gmail.com>
2015-12-01 23:01:58 -0500

    trying to fix some encoding problems that complicate the merge

--- src/main/java/com/jadventure/game/JAdventure.java
+++ src/main/java/com/jadventure/game/JAdventure.java
@@ -1,46 +1,13 @@
-package com.jadventure.game;
+package com.jadventure.game.entities;
 
-import java.util.Arrays;
+import com.jadventure.game.items.Item;
+import com.jadventure.game.items.ItemStack;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+public interface IEntity {
+	
+	public void printStorage();
+	    
+	public void addItemToStorage(Item item);
 
-import com.jadventure.game.GameModeData;
-import com.jadventure.game.GameModeData.GameMode;
-import com.jadventure.runtime.Client;
-import com.jadventure.runtime.LocalIOHandler;
-import com.jadventure.runtime.Server;
-import com.jadventure.runtime.ServiceLocator;
-import com.jadventure.game.menus.MainMenu;
-
-
-public class JAdventure {
-    
-    public static void main(String[] args) {
-        logger.info("Starting JAdventure...");
-        logger.info("Received command-line arguments: " + Arrays.toString(args));
-
-        GameModeData gameModeData = GameModeData.constructFromArguments(args);
-
-        InitializeApp(gameModeData);
-    }
-
-    private static void InitializeApp(GameModeData gameModeData) {
-        GameMode mode = gameModeData.getGameMode();
-
-        if (GameMode.CLIENT == mode) {
-            ServiceLocator.provide(new LocalIOHandler());
-            Client client = new Client(gameModeData);
-            client.listen();
-        } else if (GameMode.SERVER == mode) {
-            Server server = new Server(gameModeData);
-            server.run();
-        } else {
-            ServiceLocator.provide(new LocalIOHandler());
-            MainMenu menu = new MainMenu();
-            menu.show();
-        }
-    }
-
-    private static Logger logger = LoggerFactory.getLogger(JAdventure.class);
+    public void removeItemFromStorage(Item item);
 }
