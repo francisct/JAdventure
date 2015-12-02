b0f3725540339af7a32268d705db48851b2985e3
daBulgarian <harrison1993@gmail.com>
2015-12-01 22:00:25 -0500

    Make assertions on the state every time the player receives damage. Once the player is killed, its alive state is changed and the observer launches the prompt to determine if a new game will be played.

--- src/com/jadventure/game/TestUponDeath.java
+++ src/com/jadventure/game/TestUponDeath.java
@@ -16,10 +16,11 @@ public class TestUponDeath {
         Player player = Player.getInstance("recruit");
         player.addObserver(new DeathObserver());
 
-        System.out.println("Testing character type, health, and level.");
+        System.out.println("Testing character type, health, level, life status.");
         assertEquals(player.getCurrentCharacterType(), "Recruit");
         assertEquals(player.getHealth(), 100);
         assertEquals(player.getLevel(), 1);
+        assertEquals(player.getState(), true);
         
         System.out.println("Setting character level to 2.");
         player.setLevel(2);
@@ -29,21 +30,32 @@ public class TestUponDeath {
         Wolf wolf = new Wolf(player.getLevel());
         assertEquals(wolf.monsterType, "Wolf");
         
+        
         System.out.println("Wolf damaging the player once with 20 damage.");
         player.setHealth((int)(player.getHealth() - wolf.getDamage()));
         assertEquals(player.getHealth(), 80);
+        assertEquals(player.getState(), true); // Asserts that player is still alive
         
-        System.out.println("Wolf damaging the player a second time with 20 damage.");
+        System.out.println("Wolf damaging the player a second time with 20 damage. Total of 40 damage.");
         player.setHealth((int)(player.getHealth() - wolf.getDamage()));
         assertEquals(player.getHealth(), 60);
+        assertEquals(player.getState(), true); // Asserts that player is still alive
         
-        System.out.println("Wolf damaging the player a third time with 20 damage.");
+        System.out.println("Wolf damaging the player a third time with 20 damage. Total of 60 damage.");
         player.setHealth((int)(player.getHealth() - wolf.getDamage()));
         assertEquals(player.getHealth(), 40);
+        assertEquals(player.getState(), true); // Asserts that player is still alive
         
-        System.out.println("Wolf damaging the player a fourth time with 20 damage.");
+        System.out.println("Wolf damaging the player a fourth time with 20 damage. Total of 80 damage.");
         player.setHealth((int)(player.getHealth() - wolf.getDamage()));
-        assertEquals(player.getHealth(), 20);
+        assertEquals(player.getHealth(), 20); 
+        assertEquals(player.getState(), true); // Asserts that player is still alive
+        
+        System.out.println("Wolf damaging the player a fifth time with 20 damage. Total of 100 damage.");
+        player.setHealth((int)(player.getHealth() - wolf.getDamage()));
+        assertEquals(player.getHealth(), 0); 
+        player.stateChanged();
+        assertEquals(player.getState(), false); // Asserts that player is dead
     }
 
     @Test
