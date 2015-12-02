4ae5955f05c8157fc919144fab1e2b94f9770827
daBulgarian <harrison1993@gmail.com>
2015-12-01 20:13:16 -0500

    Replaced tab spaces with regular spaces

--- src/com/jadventure/game/UponDeathTest.java
+++ src/com/jadventure/game/UponDeathTest.java
@@ -8,19 +8,18 @@ import com.jadventure.game.entities.Player;
 import com.jadventure.game.notification.DeathObserver;
 
 public class UponDeathTest {
-	
-	@Test
-	public void testDeathRecruit() {
-		 Player player = Player.getInstance("recruit");
-		 player.addObserver(new DeathObserver());
-		 
-		 assertEquals(player.getCurrentCharacterType(), "Recruit");
-		
-		
-	}
-	
-	@Test
-	public void testDeathSewerrat() {
-	
-	}
+
+    @Test
+    public void testDeathRecruit() {
+        Player player = Player.getInstance("recruit");
+        player.addObserver(new DeathObserver());
+
+        assertEquals(player.getCurrentCharacterType(), "Recruit");
+        
+    }
+
+    @Test
+    public void testDeathSewerrat() {
+        	
+    }
 }
