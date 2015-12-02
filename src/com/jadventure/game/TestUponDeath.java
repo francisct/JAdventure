70677b967d4849b2cce8704a3f7c2f987d488b60
daBulgarian <harrison1993@gmail.com>
2015-12-01 22:12:51 -0500

    Added the sewer rat test case.

--- src/com/jadventure/game/TestUponDeath.java
+++ src/com/jadventure/game/TestUponDeath.java
@@ -56,10 +56,56 @@ public class TestUponDeath {
         assertEquals(player.getHealth(), 0); 
         player.stateChanged();
         assertEquals(player.getState(), false); // Asserts that player is dead
+        System.out.println("End Recruit test\n");
     }
 
     @Test
     public void testDeathSewerrat() {
-        	
+    	System.out.println("Creating a new player of type sewerrat and adding a DeathObserver.");
+        Player player = Player.getInstance("sewerrat");
+        player.addObserver(new DeathObserver());
+
+        System.out.println("Testing character type, health, level, life status.");
+        System.out.println(player.getCurrentCharacterType());
+        assertEquals(player.getCurrentCharacterType(), "Sewer Rat");
+        assertEquals(player.getHealth(), 100);
+        assertEquals(player.getLevel(), 1);
+        assertEquals(player.getState(), true);
+        
+        System.out.println("Setting character level to 2.");
+        player.setLevel(2);
+        assertEquals(player.getLevel(), 2);
+        
+        System.out.println("Creating new monster of type Wolf.");
+        Wolf wolf = new Wolf(player.getLevel());
+        assertEquals(wolf.monsterType, "Wolf");
+        
+        
+        System.out.println("Wolf damaging the player once with 20 damage.");
+        player.setHealth((int)(player.getHealth() - wolf.getDamage()));
+        assertEquals(player.getHealth(), 80);
+        assertEquals(player.getState(), true); // Asserts that player is still alive
+        
+        System.out.println("Wolf damaging the player a second time with 20 damage. Total of 40 damage.");
+        player.setHealth((int)(player.getHealth() - wolf.getDamage()));
+        assertEquals(player.getHealth(), 60);
+        assertEquals(player.getState(), true); // Asserts that player is still alive
+        
+        System.out.println("Wolf damaging the player a third time with 20 damage. Total of 60 damage.");
+        player.setHealth((int)(player.getHealth() - wolf.getDamage()));
+        assertEquals(player.getHealth(), 40);
+        assertEquals(player.getState(), true); // Asserts that player is still alive
+        
+        System.out.println("Wolf damaging the player a fourth time with 20 damage. Total of 80 damage.");
+        player.setHealth((int)(player.getHealth() - wolf.getDamage()));
+        assertEquals(player.getHealth(), 20); 
+        assertEquals(player.getState(), true); // Asserts that player is still alive
+        
+        System.out.println("Wolf damaging the player a fifth time with 20 damage. Total of 100 damage.");
+        player.setHealth((int)(player.getHealth() - wolf.getDamage()));
+        assertEquals(player.getHealth(), 0); 
+        player.stateChanged();
+        assertEquals(player.getState(), false); // Asserts that player is dead
+        System.out.println("End Sewer Rat test\n");
     }
 }
