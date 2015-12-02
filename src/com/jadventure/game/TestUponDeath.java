f1aa83deef2b90cee1a8a6a4fb325b5b6b340ea7
daBulgarian <harrison1993@gmail.com>
2015-12-01 20:46:42 -0500

    create new wolf

--- src/com/jadventure/game/TestUponDeath.java
+++ src/com/jadventure/game/TestUponDeath.java
@@ -5,25 +5,29 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import com.jadventure.game.entities.Player;
+import com.jadventure.game.monsters.Wolf;
 import com.jadventure.game.notification.DeathObserver;
 
 public class TestUponDeath {
     
     @Test
     public void testDeathRecruit() {
-    	System.out.println("Create a new player of type recruit and add a DeathObserver.");
+    	System.out.println("Creating a new player of type recruit and adding a DeathObserver.");
         Player player = Player.getInstance("recruit");
         player.addObserver(new DeathObserver());
 
-        System.out.println("Testing character type");
+        System.out.println("Testing character type, health, and level.");
         assertEquals(player.getCurrentCharacterType(), "Recruit");
         assertEquals(player.getHealth(), 100);
         assertEquals(player.getLevel(), 1);
         
+        System.out.println("Setting character level to 2.");
         player.setLevel(2);
         assertEquals(player.getLevel(), 2);
         
-        
+        System.out.println("Creating new monster of type Wolf.");
+        Wolf wolf = new Wolf(player.getLevel());
+        assertEquals(wolf.monsterType, "Wolf");
     }
 
     @Test
