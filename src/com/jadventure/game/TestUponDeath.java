4b9d7c7b27e615c71c04372eee48531d77d0ef1d
daBulgarian <harrison1993@gmail.com>
2015-12-01 20:36:57 -0500

    Changed name of test class from UponDeathTest to TestUponDeath.

--- src/com/jadventure/game/TestUponDeath.java
+++ src/com/jadventure/game/TestUponDeath.java
@@ -7,16 +7,23 @@ import org.junit.Test;
 import com.jadventure.game.entities.Player;
 import com.jadventure.game.notification.DeathObserver;
 
-public class UponDeathTest {
-
+public class TestUponDeath {
+    
     @Test
     public void testDeathRecruit() {
+    	System.out.println("Create a new player of type recruit and add a DeathObserver.");
         Player player = Player.getInstance("recruit");
         player.addObserver(new DeathObserver());
 
+        System.out.println("Testing character type");
         assertEquals(player.getCurrentCharacterType(), "Recruit");
         assertEquals(player.getHealth(), 100);
         assertEquals(player.getLevel(), 1);
+        
+        player.setLevel(2);
+        assertEquals(player.getLevel(), 2);
+        
+        
     }
 
     @Test
